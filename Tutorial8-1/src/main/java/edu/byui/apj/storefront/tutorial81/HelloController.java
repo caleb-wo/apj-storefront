acf1882ac1ff88530912caf53b48c7ebf4630892
caleb-wo <camawolfe52@icloud.com>
2025-02-25 16:37:21 -0700

    Added the Spring boot tutorial files.

--- Tutorial8-1/src/main/java/edu/byui/apj/storefront/tutorial81/HelloController.java
+++ Tutorial8-1/src/main/java/edu/byui/apj/storefront/tutorial81/HelloController.java
@@ -1,7 +1,9 @@
 package edu.byui.apj.storefront.tutorial81;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
     @GetMapping("/")
