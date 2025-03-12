9a445e7716d0ea195ea3fd339d30415a201f07c4
caleb-wo <camawolfe52@icloud.com>
2025-03-11 19:15:33 -0600

    Completed tutorial10-1

--- tutorial10-1/src/main/java/edu/byui/apj/storefront/tutorial101/app2/CustomerRepository.java
+++ tutorial10-1/src/main/java/edu/byui/apj/storefront/tutorial101/app2/CustomerRepository.java
@@ -2,9 +2,14 @@ package edu.byui.apj.storefront.tutorial101.app2;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     List<Customer> findByLastName(String lastName);
 
     Customer findById(long id);
+
+    @Query("SELECT c FROM Customer c WHERE LOWER(c.firstName) LIKE LOWER(CONCAT('%', :name, '%')) OR LOWER(c.lastName) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<Customer> findByFirstNameOrLastNameContainingIgnoreCase(@Param("name") String name);
 }
