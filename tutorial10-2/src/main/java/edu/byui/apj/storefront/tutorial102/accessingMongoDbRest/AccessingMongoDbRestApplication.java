1eb64507e5bbaf46813fe0eaa5a9a6112e6803b2
caleb-wo <camawolfe52@icloud.com>
2025-03-13 18:25:17 -0600

    Completed tutorial10-2. Both work independantly.

---
+++ tutorial10-2/src/main/java/edu/byui/apj/storefront/tutorial102/accessingMongoDbRest/AccessingMongoDbRestApplication.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial102.accessingMongoDbRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingMongoDbRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingMongoDbRestApplication.class, args);
+    }
+
+}
