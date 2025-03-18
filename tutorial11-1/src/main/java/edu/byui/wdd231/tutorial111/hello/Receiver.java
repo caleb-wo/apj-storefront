3004d2a60182543322701ef488db0d087bfda22f
caleb-wo <camawolfe52@icloud.com>
2025-03-18 15:59:24 -0600

    Tutorial 11-1

---
+++ tutorial11-1/src/main/java/edu/byui/wdd231/tutorial111/hello/Receiver.java
@@ -0,0 +1,14 @@
+package edu.byui.wdd231.tutorial111.hello;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        System.out.println("Received <" + email + ">");
+    }
+
+}
